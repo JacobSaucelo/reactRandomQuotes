29bbc8f1aa0a56573680a423c72e759342c4b321
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-05 20:57:49 +0800

    working fetch and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,23 +8,59 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+const baseLink = "https://api.quotable.io/quotes/random";
+
+interface QuoteDataType {
+  author: string;
+  authorSlug: string;
+  content: string;
+  dateAdded: string;
+  dateModified: string;
+  length: number;
+  tags: string[];
+  _id: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [quoteData, setQuoteData] = useState<QuoteDataType>({
+    author: "",
+    authorSlug: "",
+    content: "",
+    dateAdded: "",
+    dateModified: "",
+    length: 0,
+    tags: [],
+    _id: "",
+  });
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  const handleFetch = async () => {
+    await fetch(baseLink, { method: "GET" })
+      .then((res) => res.json())
+      .then((data: QuoteDataType[]) => {
+        setQuoteData({
+          author: data[0].author,
+          authorSlug: data[0].authorSlug,
+          content: data[0].content,
+          dateAdded: data[0].dateAdded,
+          dateModified: data[0].dateModified,
+          length: data[0].length,
+          tags: data[0].tags,
+          _id: data[0]._id,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log("quoteData: ", quoteData);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        add count {count}
-      </Button>
+      <h1>Random quotes</h1>
+      <Button onClick={handleFetch}>get a new quote</Button>
       <section>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
